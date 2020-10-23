36d144bbeb35353c120fab6d8de670cf176bb3a7
bharathramudu <shakambharicaterers@gmail.com>
2020-10-23 16:58:14 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
+
+function App(){
+  return(
+    <div class="grid-container">
+     <header>
+       <a href="/">React Shopping Cart</a>
+     </header>
+     <main>Product List</main>
+     <footer>All right is reserved</footer>
+    </div>
+  )
+}
+
+export default App;
